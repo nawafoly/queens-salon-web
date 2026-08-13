2684ac3f374651780e6fd2913d40c7018df2398c
nawafoly <114729674+nawafoly@users.noreply.github.com>
2026-08-13 22:10:13 +0300

    Export multisheet Excel V2 adapter

--- src/services/exports-v2/index.ts
+++ src/services/exports-v2/index.ts
@@ -3,5 +3,6 @@ export * from "./branding";
 export * from "./formatters";
 export * from "./file-name";
 export * from "./excel";
+export * from "./excel-multisheet";
 export * from "./pdf";
 export * from "./normalizers";
