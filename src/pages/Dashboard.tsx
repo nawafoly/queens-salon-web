bd366a0636f2e6b884af6630454a01e046159c6d
nawafoly <114729674+nawafoly@users.noreply.github.com>
2026-09-20 03:57:13 -0700

    feat(i18n): pass dashboard language to clients route

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2922,7 +2922,7 @@ const Dashboard: React.FC<DashboardProps> = ({
 
                 <Route path="tv-queue" element={<PermissionRoute permission="bookings.queue_tv.view"><DashboardQueueTv /></PermissionRoute>} />
                 <Route path="day-audit" element={<PermissionRoute permission="bookings.day_audit.manage"><DashboardDayAudit /></PermissionRoute>} />
-                <Route path="clients" element={<PermissionRoute permission="clients.view"><DashboardClients currentRole={userInfo.role} /></PermissionRoute>} />
+                <Route path="clients" element={<PermissionRoute permission="clients.view"><DashboardClients currentRole={userInfo.role} language={dashboardLanguage} /></PermissionRoute>} />
                 <Route path="partners" element={<PermissionRoute permission="partners.manage"><DashboardPartners /></PermissionRoute>} />
                 <Route path="loyalty" element={<PermissionRoute permission="clients.loyalty.manage"><DashboardLoyalty /></PermissionRoute>} />
                 <Route path="offers" element={<PermissionRoute permission="offers.manage"><DashboardOffers /></PermissionRoute>} />
